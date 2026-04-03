a0222b52e53ac92d61c48ebbfcb8dd7899367ba4
Oscar Yang <oscaryangb@gmail.com>
2026-04-03 10:03:41 +0800

    Assert entity validity

--- decs.cpp
+++ decs.cpp
@@ -1,25 +1,38 @@
 #pragma once
 
+#include <bitset>
+#include <cassert>
 #include <cstdint>
-#include <vector>
 #include <array>
+#include <vector>
 
 using EntityType = uint16_t;
+#define NULL_ENTITY 0
 constexpr size_t MAX_ENTITIES = std::numeric_limits<EntityType>::max();
 
 //#ifdef DECS_IMPLEMENTATION
-EntityType counter = 0;
-std::vector<EntityType> entities{};
+std::bitset<MAX_ENTITIES> entity_id_used{};
 
 uint64_t new_entity() {
-	auto id = counter;
-	entities.push_back(id);
-	if (counter == MAX_ENTITIES) {
-		counter = 0;
-	} else {
-		counter++;
+	EntityType id = NULL_ENTITY;
+	for (EntityType i = 1; i < entity_id_used.size(); i++) {
+		if (!entity_id_used[i]) {
+			id = i;
+			entity_id_used[i].flip();
+			break;
+		}
 	}
+	assert(id != NULL_ENTITY);
 	return id;
+}
+
+bool is_valid_entity(EntityType entity) {
+	return entity_id_used[entity];
+}
+
+void delete_entity(EntityType entity) {
+	assert(is_valid_entity(entity));
+	entity_id_used[entity] = false;
 }
 //#endif
 
@@ -35,10 +48,17 @@ struct ComponentList {
 		size++;
 		return components[size - 1];
 	}
+
+	static void remove(EntityType index) {
+		components[index] = components[size - 1];
+		entities[index] = entities[size - 1];
+		size--;
+	}
 };
 
 template<typename T>
 T* get(EntityType entity) {
+	assert(is_valid_entity(entity));
 	for (EntityType i = 0; i < ComponentList<T>::size; i++) {
 		if (ComponentList<T>::entities[i] == entity) {
 			return &ComponentList<T>::components[i];
@@ -50,30 +70,30 @@ T* get(EntityType entity) {
 
 template<typename T>
 T& add_component(EntityType entity, T&& component) {
+	assert(is_valid_entity(entity));
 	return ComponentList<T>::push_back(entity, std::forward<T>(component));
+}
+
+template<typename T>
+void remove_component(EntityType entity) {
+	assert(is_valid_entity(entity));
+	for (EntityType i = 1; i < ComponentList<T>::size; i++) {
+		if (ComponentList<T>::entities[i] == entity) {
+			ComponentList<T>::remove(i);
+			return;
+		}
+	}
 }
 
 template<typename T> // TODO Use fold expressions to iterate upon variadic templates
 std::vector<T*> get_components() {
 	std::vector<T*> result{};
-	for (EntityType i = 0; i < ComponentList<T>::size; i++) {
+	for (EntityType i = 1; i < ComponentList<T>::size; i++) {
+		if (!is_valid_entity(ComponentList<T>::entities[i])) {
+			ComponentList<T>::remove(i);
+			if (i >= ComponentList<T>::size) break;
+		}
 		result.push_back(&ComponentList<T>::components[i]);
 	}
 	return result;
-}
-
-template<typename T>
-void remove_component(EntityType entity) {
-	for (EntityType i = 0; i < ComponentList<T>::size; i++) {
-		if (ComponentList<T>::entities[i] == entity) {
-			ComponentList<T>::components[i] = ComponentList<T>::components[ComponentList<T>::size - 1];
-			ComponentList<T>::entities[i] = ComponentList<T>::entities[ComponentList<T>::size - 1];
-			ComponentList<T>::size--;
-		}
-	}
-}
-
-template<typename T>
-void delete_entity(EntityType entity) {
-	// TODO
 }
--- example.cpp
+++ example.cpp
@@ -6,15 +6,27 @@ struct Position {
 	float y{};
 };
 
+struct Color {
+	uint8_t r{};
+	uint8_t g{};
+	uint8_t b{};
+};
+
+
 int main() {
 	auto entity = new_entity();
 	add_component(entity, Position{.x = 5.f});
 
 	auto entity_2 = new_entity();
 	add_component(entity_2, Position{.x = 6.f});
+	add_component(entity_2, Color{.r = 125});
 
 	for (auto component : get_components<Position>()) {
 		std::cout << component->x << "\n";
+	}
+
+	for (auto component : get_components<Color>()) {
+		std::cout << static_cast<int>(component->r) << "\n";
 	}
 
 	std::cout << sizeof(ComponentList<Position>::components) << "\n";
