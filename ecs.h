a3536017f14fced1a9bb2d91d9a755d2bc806ddb
oscaryangb <oscaryangb@gmail.com>
2026-05-30 14:21:31 -0400

    Fix bugs and add singletons

--- ecs.h
+++ ecs.h
@@ -5,14 +5,15 @@
 #include <cstdint>
 #include <array>
 #include <vector>
+#include <algorithm>
 
 using EntityType = uint16_t;
 #define NULL_ENTITY 0
-constexpr EntityType MAX_ENTITIES = std::numeric_limits<EntityType>::max();
+constexpr EntityType MAX_ENTITIES = 1000;
 
 inline std::bitset<MAX_ENTITIES> entity_id_used{};
 
-inline uint64_t new_entity() {
+inline EntityType new_entity() {
 	EntityType id = NULL_ENTITY;
 	for (EntityType i = 1; i < entity_id_used.size(); i++) {
 		if (!entity_id_used[i]) {
@@ -38,87 +39,137 @@ template<typename T>
 struct ComponentList {
 	using Pair = std::tuple<EntityType, T>;
 	inline static std::array<Pair, MAX_ENTITIES> data;
-	inline static std::array<T*, MAX_ENTITIES> entity_component_map;
+	inline static std::array<EntityType, MAX_ENTITIES> entity_to_data_index_map;
 	inline static EntityType size = 1;
 
 	static EntityType get_entity(EntityType index) {
 		return std::get<EntityType>(data[index]);
 	}
 
+	static T* get_component(EntityType index) {
+		return &std::get<T>(data[index]);
+	}
+
+	static bool has_entity(EntityType entity) {
+		return entity_to_data_index_map[entity] != 0;
+	}
+
 	static T& push_back(EntityType entity, T&& component) {
 		data[size] = {entity, component};
-		entity_component_map[entity] = &std::get<T>(data[size]);
+		entity_to_data_index_map[entity] = size;
 		size++;
 		return std::get<T>(data[size - 1]);
 	}
 
 	static void remove(EntityType index) {
-		entity_component_map[std::get<EntityType>(data[index])] = nullptr;
+		entity_to_data_index_map[std::get<EntityType>(data[index])] = 0;
 		data[index] = std::move(data[size - 1]);
 		size--;
 	}
 
 	template<typename Function>
-	static void sort(Function&& function) { // TODO Bugged!!
-		std::sort(data.begin(), data.begin() + size, [function](const Pair& a, const Pair& b) { return function(std::get<T>(a), std::get<T>(b)); });
-		entity_component_map = {};
+	static void sort(Function&& function) {
+		std::sort(data.begin() + 1, data.begin() + size, [function](const Pair& a, const Pair& b) {
+			return function(std::get<T>(a), std::get<T>(b));
+		});
+
+		entity_to_data_index_map = {};
 		for (int i = 1; i < size; i++) {
-			entity_component_map[std::get<EntityType>(data[i])] = &std::get<T>(data[i]);
+			entity_to_data_index_map[std::get<EntityType>(data[i])] = i;
 		}
 	}
 };
 
 template<typename T, typename Function>
 void sort(Function&& function) {
-	ComponentList<T>::sort(function);
+	ComponentList<T>::sort(std::forward<Function>(function));
 }
 
 template<typename T>
 T* get_component(EntityType entity) {
 	assert(is_valid_entity(entity));
-	return ComponentList<T>::entity_component_map[entity];
+	assert(ComponentList<T>::has_entity(entity));
+	return ComponentList<T>::get_component(ComponentList<T>::entity_to_data_index_map[entity]);
 }
+
+template<typename T>
+T* try_get_component(EntityType entity) {
+	assert(is_valid_entity(entity));
+	return ComponentList<T>::has_entity(entity) ? ComponentList<T>::get_component(ComponentList<T>::entity_to_data_index_map[entity]) : nullptr;
+}
+
 
 template<typename T>
 T& add_component(EntityType entity, T&& component) {
 	assert(is_valid_entity(entity));
+	assert(!ComponentList<T>::has_entity(entity));
 	return ComponentList<T>::push_back(entity, std::forward<T>(component));
 }
 
 template<typename T>
 void remove_component(EntityType entity) {
 	assert(is_valid_entity(entity));
-	for (EntityType i = 1; i < ComponentList<T>::size; i++) {
-		if (ComponentList<T>::get_entity(i) == entity) {
-			ComponentList<T>::remove(i);
-			return;
-		}
-	}
+	assert(ComponentList<T>::has_entity(entity));
+	ComponentList<T>::remove(ComponentList<T>::entity_to_data_index_map[entity]);
 }
 
 template<typename... T>
 std::vector<std::tuple<EntityType, T*...>> get_entities() {
-	std::bitset<MAX_ENTITIES> found_entities{};
-	found_entities.set();
+	std::vector<std::tuple<EntityType, T*...>> result{};
 
-	([&found_entities](){ // TODO Can optimize this by checking the entity component map for nullptr?
-		std::bitset<MAX_ENTITIES> current_found_entities{};
-		for (EntityType i = 1; i < ComponentList<T>::size; i++) {
-			if (!is_valid_entity(ComponentList<T>::get_entity(i))) {
-				ComponentList<T>::remove(i);
-				if (i >= ComponentList<T>::size) break;
+	bool first_iteration = true;
+	([&result, &first_iteration]() {
+		if (first_iteration) {
+			for (EntityType i = 1; i < ComponentList<T>::size; i++) {
+				while (!is_valid_entity(ComponentList<T>::get_entity(i)) && i < ComponentList<T>::size) {
+					ComponentList<T>::remove(i);
+				}
+				if (i >= ComponentList<T>::size) {
+					break;
+				}
+				result.emplace_back(ComponentList<T>::get_entity(i), ComponentList<T>::get_component(i)...);
 			}
-			current_found_entities[ComponentList<T>::get_entity(i)] = true;
 		}
-		found_entities &= current_found_entities;
+		first_iteration = false;
 	}(), ...);
 
-	std::vector<std::tuple<EntityType, T*...>> result{};
-	for (EntityType i = 0; i < found_entities.size(); i++) {
-		if (found_entities[i]) {
-			result.push_back({i, get_component<T>(i) ...});
-		}
-	}
+	result.erase(std::remove_if(result.begin(), result.end(), [&result](const auto& pair) {
+		bool found = true;
+		found &= ([&pair]() {
+			return ComponentList<T>::has_entity(std::get<EntityType>(pair));
+		}(), ...);
+		return !found;
+	}), result.end());
 
 	return result;
+}
+
+template<typename T>
+struct Singleton {
+	static T object;
+	inline static bool exists = false;
+};
+
+template<typename T>
+T* get_singleton() {
+	assert(Singleton<T>::exists);
+	return &Singleton<T>::object;
+}
+
+template<typename T>
+T* try_get_singleton() {
+	return Singleton<T>::exists ? &Singleton<T>::object : nullptr;
+}
+
+template<typename T>
+void create_singleton(T&& object) {
+	assert(!Singleton<T>::exists);
+	Singleton<T>::exists = true;
+	Singleton<T>::object = object;
+}
+
+template<typename T>
+void destroy_singleton() {
+	assert(Singleton<T>::exists);
+	Singleton<T>::exists = false;
 }
--- example.cpp
+++ example.cpp
@@ -15,10 +15,19 @@ struct Color {
 int main() {
 	auto entity = new_entity();
 	add_component(entity, Position{.x = 5.f});
+	add_component(entity, Color{.r = 127});
 
 	auto entity_2 = new_entity();
 	add_component(entity_2, Position{.x = 6.f});
 	add_component(entity_2, Color{.r = 125});
+
+	auto entity_4 = new_entity();
+	add_component(entity_4, Position{.x = 100.f});
+	add_component(entity_4, Color{.r = 1});
+
+	auto entity_3 = new_entity();
+	add_component(entity_3, Position{.x = 10.f});
+	add_component(entity_3, Color{.r = 2});
 
 	for (auto [entity, position] : get_entities<Position>()) {
 		std::cout << position->x << "\n";
@@ -28,12 +37,11 @@ int main() {
 		std::cout << static_cast<int>(color->r) << "\n";
 	}
 
-	sort<Position>([](const Position& first, const Position& second) { return first.x > second.y; });
+	sort<Position>([](const Position& first, const Position& second) { return first.x > second.x; });
 
 	for (auto [entity, position] : get_entities<Position>()) {
 		std::cout << position->x << "\n";
 	}
-
 
 	return 0;
 }
